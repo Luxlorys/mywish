3ad4f379614cc7bfd4a255d8dfa50c6234e15241
Luxlorys <bossbyrcev221@gmail.com>
2023-12-11 16:07:08 +0200

    sign up view is responsive now

--- client/src/views/SignUp.tsx
+++ client/src/views/SignUp.tsx
@@ -1,6 +1,7 @@
-import { Button, FormControl, Input, Text } from "@chakra-ui/react";
+import { Button, Center, Divider, FormControl, Image, Input, Text } from "@chakra-ui/react";
 import '../styles/signup.css';
-import { ArrowForwardIcon, CheckCircleIcon } from "@chakra-ui/icons";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import gmailLogo from '/assets/img/gmail_logo.png';
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
@@ -10,7 +11,7 @@ export default function SignUp() {
                 <div className="left-content">
                     <Text as='b'> #1 Create an acoount</Text>
                     <Text as='b'>🛡️</Text>
-
+                    
                     <Text as='b'>#2 Fill your wish list</Text>
                     <Text as='b'>📝</Text>
 
@@ -29,40 +30,37 @@ export default function SignUp() {
 
             <div className="right-container">
                 <div className="right-content">
-
                     <Text fontSize='3xl' color='teal.600'>Create an account</Text>
                     <FormControl mt={5}>
-                        {/* <FormLabel>Username</FormLabel> */}
                         <Input placeholder="username "/>
                     </FormControl>
 
                     <FormControl mt={5}>
-                        {/* <FormLabel>Email</FormLabel> */}
                         <Input placeholder="email"/>
                     </FormControl>
 
                     <FormControl mt={5}>
-                        {/* <FormLabel>Password</FormLabel> */}
                         <Input type="password" placeholder="password"/>
                         <Input mt={5} type="password" placeholder="repeat password" />
                     </FormControl>
-                    {/* <Center> */}
+
+                    
                     <Button
                         rightIcon={<ArrowForwardIcon />}
                         mt={5}
-                        // w='20vw'
                         variant='outline'
                         colorScheme="teal">
                             sign up
                     </Button>
 
-                    <Button mt={5}
-                        variant='ghost'
-                        colorScheme="teal"
-                        rightIcon={<CheckCircleIcon />}>
-                            <Link to='/signin'>sign in</Link>
+                    <Divider mt={7} />
+
+                    <Button mt={5}>
+                        <Image src={gmailLogo} alt="gmail logo" w={6} h={5} mr={2} />
+                        or sign up with google
                     </Button>
-                    {/* </Center> */}
+
+                    <Text mt={5}>have an account? <Link className="link" color="red" to='/signin'>sign in</Link></Text>
                 </div>
             </div>
         </div>
